d3e1f9d56b200d5c5652aaa08f266be5c07fd869
camolezi <camolezi@usp.br>
2021-04-10 22:24:51 -0300

    Created test for when asking for permission when record button is first clicked

--- src/views/RecordView.test.tsx
+++ src/views/RecordView.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
+import userEvent from '@testing-library/user-event';
 import RecordView from './RecordView';
 import WrapWithProviders from '../app/Providers';
 
@@ -8,10 +9,30 @@ function renderRecordView() {
   render(WrapWithProviders(<RecordView />));
 }
 
+function createNavigatorMock() {
+  const navigatorMock = {
+    mediaDevices: {
+      getUserMedia: jest.fn(),
+    },
+  };
+  jest.spyOn(window, 'navigator', 'get').mockReturnValue(navigatorMock as any);
+
+  return navigatorMock;
+}
+
 test('RecordView should have a start record button', () => {
   renderRecordView();
+  const recordButton = screen.getByRole('button', { name: /Start Record/i });
+  expect(recordButton).toBeVisible();
+});
+
+test('Should ask for permission when record button is first clicked', () => {
+  renderRecordView();
+
+  const navigatorMock = createNavigatorMock();
 
   const recordButton = screen.getByRole('button', { name: /Start Record/i });
+  userEvent.click(recordButton);
 
-  expect(recordButton).toBeVisible();
+  expect(navigatorMock.mediaDevices.getUserMedia).toBeCalledTimes(1);
 });
