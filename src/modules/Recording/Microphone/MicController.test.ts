f2f3cc2890182a8cb1c36cc9752ca59e0a287f1d
camolezi <camolezi@usp.br>
2021-04-11 19:25:08 -0300

    Created tests for Microphone controller

--- src/modules/Recording/Microphone/MicController.test.ts
+++ src/modules/Recording/Microphone/MicController.test.ts
@@ -1,17 +1,41 @@
+/* eslint-disable func-names */
 import MicrophoneController from './MicController';
 
-function createMediaRecorderMock() {
-  return { start: jest.fn(), stop: jest.fn(), ondataavailable: jest.fn() };
+function createMediaRecorderMock(): MediaRecorder {
+  return ({
+    start: jest.fn(),
+    stop: jest.fn(function (this: MediaRecorder) {
+      if (this.onstop) this.onstop(new Event(''));
+    }),
+  } as unknown) as MediaRecorder;
 }
 
 test('Should start and stop the browser microphone', () => {
   const mediaRecorder = createMediaRecorderMock();
 
-  const micController = new MicrophoneController(mediaRecorder as any);
+  const micController = new MicrophoneController(mediaRecorder);
 
   micController.start();
   expect(mediaRecorder.start).toHaveBeenCalledTimes(1);
 
   micController.stop();
   expect(mediaRecorder.stop).toHaveBeenCalledTimes(1);
+});
+
+test('Should store all received blobs', () => {
+  const mediaRecorder = createMediaRecorderMock();
+  const blob = { data: new Blob(['a', 'b', 'c']) } as BlobEvent;
+
+  const micController = new MicrophoneController(mediaRecorder);
+  micController.start();
+
+  expect(mediaRecorder.ondataavailable).toBeDefined();
+
+  (mediaRecorder as any).ondataavailable(blob);
+  (mediaRecorder as any).ondataavailable(blob);
+
+  micController.stop();
+
+  const data = micController.getRecordedData();
+  expect(data).toEqual([blob.data, blob.data]);
 });
--- src/modules/Recording/Microphone/MicController.ts
+++ src/modules/Recording/Microphone/MicController.ts
@@ -1,14 +1,27 @@
 export default class MicrophoneController {
   private readonly mediaRecorder: MediaRecorder;
 
-  private dataChunks: Blob[] = new Array<Blob>();
+  private readonly dataChunks: Blob[];
 
   constructor(mediaRecorder: MediaRecorder) {
     this.mediaRecorder = mediaRecorder;
+    this.dataChunks = new Array<Blob>();
 
     this.mediaRecorder.ondataavailable = (event: BlobEvent) => {
       this.dataChunks.push(event.data);
     };
+
+    // this.mediaRecorder.onstop = (_event: any) => {
+    //   console.log('onStop');
+    //   const audio = document.createElement('audio');
+    //   audio.controls = true;
+    //   const blob = new Blob(this.dataChunks, {
+    //     type: 'audio/ogg; codecs=opus',
+    //   });
+    //   const audioURL = window.URL.createObjectURL(blob);
+    //   audio.src = audioURL;
+    //   document.body.appendChild(audio);
+    // };
   }
 
   start(): void {
@@ -17,6 +30,9 @@ export default class MicrophoneController {
 
   stop(): void {
     this.mediaRecorder.stop();
-    console.log(this.dataChunks);
+  }
+
+  getRecordedData(): Blob[] {
+    return this.dataChunks;
   }
 }
