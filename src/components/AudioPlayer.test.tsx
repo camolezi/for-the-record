cfec3501f58805f0e2f5ef083286f4939e54b259
camolezi <camolezi@usp.br>
2021-04-12 23:04:07 -0300

    Created icon buttons for Play/Stop/Rwd/Fwd

--- src/components/AudioPlayer.test.tsx
+++ src/components/AudioPlayer.test.tsx
@@ -3,11 +3,11 @@ import { render, screen } from '@testing-library/react';
 import AudioPlayer from './AudioPlayer';
 
 describe('Control buttons should be visable', () => {
-  test.each(['Play', 'Stop', 'Rwd', 'Fwd'])(
+  test.each(['PlayPause', 'Stop', 'Rewind', 'Forward'])(
     'button %p should be visible',
     (name) => {
       render(<AudioPlayer audioSrc="" />);
-      const controlButton = screen.getByRole('button', { name });
+      const controlButton = screen.getByRole('button', { name, exact: false });
       expect(controlButton).toBeVisible();
     }
   );
--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,12 +1,46 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 
+import { ButtonGroup, Icon, IconButton } from '@chakra-ui/react';
+
+import { MdForward10, MdReplay10, MdStop, MdPlayArrow } from 'react-icons/md';
+
 interface Props {
   audioSrc: string;
 }
 
 function AudioPlayer({ audioSrc }: Props): JSX.Element {
-  return <audio controls src={audioSrc} />;
+  return (
+    <>
+      <ButtonGroup variant="outline" spacing="3">
+        <IconButton
+          colorScheme="teal"
+          aria-label="Forward"
+          size="lg"
+          icon={<Icon as={MdForward10} />}
+        />
+        <IconButton
+          colorScheme="teal"
+          aria-label="PlayPause"
+          size="lg"
+          icon={<Icon as={MdPlayArrow} />}
+        />
+        <IconButton
+          colorScheme="teal"
+          aria-label="Stop"
+          size="lg"
+          icon={<Icon as={MdStop} />}
+        />
+        <IconButton
+          colorScheme="teal"
+          aria-label="Rewind"
+          size="lg"
+          icon={<Icon as={MdReplay10} />}
+        />
+      </ButtonGroup>
+      <audio controls src={audioSrc} />
+    </>
+  );
 }
 
 export default AudioPlayer;
