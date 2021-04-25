4e23ef46ec99b22b45c89422489998fcd0b5fd2e
camolezi <camolezi@usp.br>
2021-04-25 13:49:23 -0300

    Added storybook as a dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,13 @@ module.exports = {
   extends: ['airbnb-typescript-prettier'],
   plugins: ['testing-library', 'cypress'],
   rules: {
+    'import/no-extraneous-dependencies': [
+      'warn',
+      {
+        devDependencies: false,
+      },
+    ],
+    'react/jsx-props-no-spreading': ['warn'],
     '@typescript-eslint/ban-types': ['warn'],
     'no-use-before-define': 'off',
     'import/prefer-default-export': ['warn'],
