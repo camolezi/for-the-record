6288ee5745a52a3a83a06ba550fd104625642d2f
camolezi <camolezi@usp.br>
2021-05-02 22:45:30 -0300

    Created button component for month change in calendar

---
+++ src/components/Calendar/MonthCalendar/MonthButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { motion } from 'framer-motion';
+import { Button, ButtonProps } from '@chakra-ui/react';
+
+const MotionButton = motion(Button);
+
+export default function MonthButton({
+  children,
+  ...props
+}: ButtonProps): JSX.Element {
+  return (
+    <MotionButton
+      colorScheme="gray"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.6, transition: { duration: 0.1 } }}
+      {...props}
+    >
+      {children}
+    </MotionButton>
+  );
+}
