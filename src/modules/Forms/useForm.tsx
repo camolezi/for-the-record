e85acb30b5c28675b1145e4ded8483341eee3456
camolezi <camolezi@usp.br>
2021-05-30 11:37:19 -0300

    Fixed use form hook, where not all fields would re-validate when form state changed

--- src/modules/Forms/useForm.tsx
+++ src/modules/Forms/useForm.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import _ from 'lodash';
 
 export interface Validation {
-  readonly [id: string]: (newValue: string) => null | string;
+  readonly [id: string]: (
+    newValue: string,
+    formState: FormState
+  ) => null | string;
 }
-export interface FormInputState {
+
+export interface FormState {
   readonly [id: string]: {
     readonly error: string | null;
     readonly value: string;
@@ -13,21 +17,39 @@ export interface FormInputState {
 
 function useForm(
   validation: Validation
-): readonly [(e: React.FormEvent<HTMLFormElement>) => void, FormInputState] {
-  const [formState, changeState] = useState<FormInputState>(
+): readonly [(e: React.FormEvent<HTMLFormElement>) => void, FormState] {
+  const [formState, changeState] = useState<FormState>(
     _.mapValues(validation, () => ({ error: null, value: '' }))
   );
 
-  const onChange = (e: React.FormEvent<HTMLFormElement>) => {
-    const inputElement = e.target as HTMLInputElement;
+  const validateFormFields = useCallback(
+    (currentformState: FormState) => {
+      const newFormState = Object.entries(validation).reduce<FormState>(
+        (result, [id, validationFunc]) => {
+          if (validationFunc) {
+            const { value } = currentformState[id];
+            const newValidationState = validationFunc(value, currentformState);
+            return { ...result, [id]: { error: newValidationState, value } };
+          }
+          return result;
+        },
+        {}
+      );
+      changeState({ ...currentformState, ...newFormState });
+    },
+    [validation]
+  );
 
-    const { id, value } = inputElement;
-    const validationFunction = validation[id] ?? null;
-    if (validationFunction) {
-      const newValidationState = validationFunction(value);
-      changeState({ ...formState, [id]: { error: newValidationState, value } });
-    }
-  };
+  const onChange = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      const inputElement = e.target as HTMLInputElement;
+
+      const { id, value } = inputElement;
+      validateFormFields({ ...formState, [id]: { ...formState[id], value } });
+    },
+    [formState, validateFormFields]
+  );
+
   return [onChange, formState] as const;
 }
 
