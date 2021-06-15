2975574fdbd0d803ca087aa8785854e53da9d20e
camolezi <camolezi@usp.br>
2021-06-15 08:51:48 -0300

    Created AuthSession class, responsible for managing user Authentication info in Session storage

---
+++ src/modules/Authentication/AuthSession.ts
@@ -0,0 +1,55 @@
+import { CryptoKeyToStr } from '../Crypto/Authenticate';
+import { userdb } from '../Db/Databases';
+import { User } from '../Db/types';
+import UserDb from '../Db/UserDb';
+import { AuthenticateUser, CreateNewUser } from './Login';
+
+export class AuthSession {
+  private storage: Storage;
+
+  private users: UserDb;
+
+  private readonly authInfo = 'authInfo';
+
+  constructor(storage: Storage, users: UserDb) {
+    this.storage = storage;
+    this.users = users;
+  }
+
+  private getAuthSession() {
+    return this.storage.getItem(this.authInfo);
+  }
+
+  private setAuthSession(sessionInfo: string) {
+    this.storage.setItem(this.authInfo, sessionInfo);
+  }
+
+  isAuthenticated(): boolean {
+    return this.getAuthSession() !== null;
+  }
+
+  // getUserKey(): MaybeAsync<CryptoKey> {
+  // //  return this.getAuthSession();
+  // // TODO create function to convert string to crypto key
+  // }
+
+  authenticate(password: string): Promise<boolean> {
+    return this.users
+      .getUser()
+      .chain((user) => AuthenticateUser(user, password))
+      .chain(CryptoKeyToStr)
+      .ifJust((key) => this.setAuthSession(key))
+      .run()
+      .then((result) => result.isJust());
+  }
+
+  createNewUser(name: string, password: string): Promise<User | null> {
+    return CreateNewUser(name, password)
+      .chain((user) => this.users.createUser(user))
+      .run()
+      .then((maybe) => maybe.extractNullable());
+  }
+}
+
+const authSession = new AuthSession(window.sessionStorage, userdb);
+export default authSession;
