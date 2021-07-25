5d4e316fc1242650bbf7be5f2de357881562dd69
camolezi <camolezi@usp.br>
2021-07-25 15:01:41 -0300

    Added error styles and success redirect for login and create user page

--- src/views/CreateUserView.tsx
+++ src/views/CreateUserView.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import { Container } from '@chakra-ui/react';
+import { Container, Heading, SlideFade } from '@chakra-ui/react';
 
+import { Navigate } from 'react-router-dom';
 import UserCreationForm from '../modules/Authentication/UserCreation/UserCreationForm';
 
 import { selectIsUserCreated } from '../modules/Authentication/state/UserSelectors';
 import { useTypedSelector } from '../app/Store';
 import { AsyncActionStatus } from '../utils/ReduxUtils/AsyncActionHelpers';
 import RenderAsyncActions from '../utils/ReduxUtils/RenderAsyncActions';
+import LoadingSpinner from '../components/Loading/LoadingSpinner';
 
 function CreateUserView(): JSX.Element {
   const isUserCreated = useTypedSelector(selectIsUserCreated);
+
+  const failedErrorMessage = (
+    <SlideFade in offsetY="-50px">
+      <Heading
+        py={10}
+        as="h6"
+        size="xs"
+        pb={5}
+        color="red.300"
+        textAlign="center"
+      >
+        Failed to create user :(. Please reload and try again.
+      </Heading>
+    </SlideFade>
+  );
+
   return (
     <Container maxW="container.sm">
       <RenderAsyncActions
         actionStatus={isUserCreated}
         statusMap={{
           [AsyncActionStatus.NotStarted]: <UserCreationForm />,
-          [AsyncActionStatus.Completed]: <h1>Successfully Created</h1>,
-          [AsyncActionStatus.Rejected]: <h1>Failed to create user</h1>,
-          [AsyncActionStatus.Pending]: <h1>Pending</h1>,
+          [AsyncActionStatus.Completed]: <Navigate to="/" replace />,
+          [AsyncActionStatus.Rejected]: failedErrorMessage,
+          [AsyncActionStatus.Pending]: <LoadingSpinner />,
         }}
       />
     </Container>
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -4,6 +4,7 @@ import {
   Container,
   Heading,
   Link as ChakraLink,
+  SlideFade,
 } from '@chakra-ui/react';
 import { Link, Navigate } from 'react-router-dom';
 import UserLoginForms from '../modules/Authentication/UserLogin/UserLoginForms';
@@ -14,23 +15,39 @@ import {
   selectIsUserLoggedIn,
 } from '../modules/Authentication/state/UserSelectors';
 import { AsyncActionStatus } from '../utils/ReduxUtils/AsyncActionHelpers';
+import LoadingSpinner from '../components/Loading/LoadingSpinner';
 
 function LoginView(): JSX.Element {
   const isUsedloggedIn = useTypedSelector(selectIsUserLoggedIn);
   const isUserCreated = useTypedSelector(selectIsUserCreated);
 
+  const withIncorrectPassword = (children: React.ReactNode) => (
+    <>
+      <SlideFade in offsetY="-50px">
+        <Heading as="h6" size="xs" pb={5} color="red.300">
+          Incorrect Password
+        </Heading>
+        {children}
+      </SlideFade>
+    </>
+  );
+
+  const userLoginForm = (
+    <RenderAsyncActions
+      actionStatus={isUsedloggedIn}
+      statusMap={{
+        [AsyncActionStatus.NotStarted]: <UserLoginForms />,
+        [AsyncActionStatus.Completed]: <Navigate to="/" replace />,
+        [AsyncActionStatus.Rejected]: withIncorrectPassword(<UserLoginForms />),
+        [AsyncActionStatus.Pending]: <LoadingSpinner />,
+      }}
+    />
+  );
+
   return (
-    <Container maxW="container.sm">
+    <Container maxW="container.sm" mt={5}>
       {isUserCreated === AsyncActionStatus.Completed ? (
-        <RenderAsyncActions
-          actionStatus={isUsedloggedIn}
-          statusMap={{
-            [AsyncActionStatus.NotStarted]: <UserLoginForms />,
-            [AsyncActionStatus.Completed]: <Navigate to="/" replace />,
-            [AsyncActionStatus.Rejected]: <h1>Incorrect Password</h1>,
-            [AsyncActionStatus.Pending]: <h1>Pending</h1>,
-          }}
-        />
+        userLoginForm
       ) : (
         <Center textAlign="center">
           <Heading as="h2" size="lg">
