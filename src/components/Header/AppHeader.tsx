9d5dd93ea935bf4499a34433929935091cbf4532
camolezi <camolezi@usp.br>
2021-07-31 13:15:38 -0300

    Added new animation and style on app header

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -1,20 +1,68 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Flex } from '@chakra-ui/react';
-import ColorModeButton from './ColorModeButton';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Flex, Heading } from '@chakra-ui/react';
+import { AnimateSharedLayout } from 'framer-motion';
+import MotionCenter from '../Motion/MotionCenter';
 
 function AppHeader(): JSX.Element {
-  return (
-    <Flex boxSize="100%" align="center" justify="space-around">
-      <Button as="div">
-        <Link to="calendar">Calendar</Link>
-      </Button>
+  const navigate = useNavigate();
+  const location = useLocation();
+  const notSelectedBackgroundColor = 'whiteAlpha.100';
+  const selectedBackgroudColor = 'blue.700';
 
-      <Button as="div">
-        <Link to="/">Recording</Link>
-      </Button>
-      <ColorModeButton />
-    </Flex>
+  const calendarPath = '/calendar';
+  const recordingPath = '/';
+  const optionsPath = '/options';
+
+  const wrapDecorationIfSelected = (isSelected: boolean): JSX.Element => {
+    if (isSelected)
+      return (
+        <MotionCenter
+          boxSize="100%"
+          layout
+          layoutId="outline"
+          initial={false}
+          transition={{ type: 'spring' }}
+          backgroundColor={selectedBackgroudColor}
+          zIndex={-1}
+        />
+      );
+
+    return <></>;
+  };
+
+  const MenuItem = (path: string, text: string) => (
+    <MotionCenter
+      as="button"
+      onTapStart={() => {
+        navigate(path);
+      }}
+      backgroundColor={notSelectedBackgroundColor}
+      textAlign="center"
+      flex="1"
+      role="link"
+    >
+      <Heading position="absolute" as="h4" size="md">
+        {text}
+      </Heading>
+
+      {wrapDecorationIfSelected(path === location.pathname)}
+    </MotionCenter>
+  );
+
+  return (
+    <AnimateSharedLayout>
+      <Flex
+        boxSize="100%"
+        align="stretch"
+        justify="space-around"
+        overflow="hidden"
+      >
+        {MenuItem(calendarPath, 'Calendar')}
+        {MenuItem(recordingPath, 'Recording')}
+        {MenuItem(optionsPath, 'Options')}
+      </Flex>
+    </AnimateSharedLayout>
   );
 }
 
