1d8dba9a14269e6bcbbe25332d8a0b91e38e91e6
camolezi <camolezi@usp.br>
2021-08-20 20:43:56 -0300

    Removed selection default style on google chrome

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
 import themeConfig from './config';
 
-const AppTheme = extendTheme({ config: themeConfig });
+const AppTheme = extendTheme({
+  config: themeConfig,
+  styles: {
+    global: {
+      '*': {
+        '-webkit-tap-highlight-color': 'transparent !important',
+      },
+    },
+  },
+});
 
 export default AppTheme;
