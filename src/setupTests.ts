f27ba7f8605392caf15c3f33c8367321296b0945
camolezi <camolezi@usp.br>
2021-09-16 21:45:50 -0300

    Fixed SVG Path error in Jest Test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,9 @@
+/* eslint-disable max-classes-per-file */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Render Framer motion svg animation on jest dom
+global.SVGElement.prototype.getTotalLength = () => 1;
